587c6dacf282419581c3f127ced682cb4bca102a
Cristiano Nascimento <cristnascimento@gmail.com>
2020-08-23 22:26:41 -0300

    Adding simple spring initializr project into eclipse

---
+++ h2-model-app/h2model/src/main/java/com/github/cristnascimento/h2model/H2modelApplication.java
@@ -0,0 +1,14 @@
+package com.github.cristnascimento.h2model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2modelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2modelApplication.class, args);
+		System.out.println("Hello everyone ");
+	}
+
+}
---
+++ h2-model-app/h2model/src/test/java/com/github/cristnascimento/h2model/H2modelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cristnascimento.h2model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2modelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
